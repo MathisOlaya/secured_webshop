1781eb110f34c81a279a1b43335c581e80928f1a
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-01-13 14:54:21 +0100

    feat: ajout de chemin de connexion

--- app/server.js
+++ app/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const https = require("https");
 const fs = require('fs');
-
+const path = require('path');
+const mysql = require('mysql2')
 
 const app = express();
 
@@ -11,10 +12,42 @@ const options = {
     cert: fs.readFileSync('certs/cert.crt')
 }
 
+//create connection with db
+// mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'root',
+//     database: 'db_secured_webshop',
+//     port: 6033
+// });
+
+//try to connect
+// connection.connect((err) => {
+//     if (err) {
+//       console.error('Erreur de connexion à la base de données:');
+//       return;
+//     }
+//   });
+
 const userRoute = require('./routes/User');
 app.use('/user', userRoute);
 
+app.use(express.static(path.join(__dirname, 'public')));
 
+//default page
+app.get('/', (req, res) => {
+    res.redirect('/login');
+})
+
+//signup page
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'signup', 'signup.html'));
+  });
+
+//login page (default)
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login', 'login.html'));
+  });
 
 // Démarrage du serveur
 https.createServer(options, app).listen(443, () => {
