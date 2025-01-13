6dc3c859a4b9522fd17e1f03dad3eb2b5ae22bc9
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-01-13 13:49:23 +0100

    feat: use certificate

--- app/server.js
+++ app/server.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const https = require("https");
+const fs = require('fs');
 
 
 const app = express();
+
+//add certification file
+const options = {
+    key: fs.readFileSync('certs/key.pem'),
+    cert: fs.readFileSync('certs/cert.crt')
+}
+
 const userRoute = require('./routes/User');
 app.use('/user', userRoute);
 
 
 
 // Démarrage du serveur
-app.listen(443, () => {
+https.createServer(options, app).listen(443, () => {
     console.log('Server running on port 443');
 });
