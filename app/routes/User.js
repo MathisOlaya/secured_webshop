fabbd339aa02a5411797a5584fdc61461288269d
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-01-20 14:40:55 +0100

    feat: créer une nouvelle route ayant comme paramètre son username

--- app/routes/User.js
+++ app/routes/User.js
@@ -3,9 +3,10 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/UserController");
 
-router.get("/", controller.getSignUp);
+router.get("/:username", controller.getSignUp);
 
-//user signup
+//user signup nd login
 router.post("/signup", controller.postSignUp);
+router.post("/login", controller.postLogin);
 
 module.exports = router;
