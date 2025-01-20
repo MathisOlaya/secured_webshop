45da02711bd3da4789fea2fa078f62ca4b255556
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-01-20 14:39:26 +0100

    feat: vérifier si l'utilisateur à rentré les bonnes informations, et retourner un bool

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -36,4 +36,37 @@ async function postSignUp(req, res) {
   });
 }
 
-module.exports = { getSignUp, postSignUp };
+async function postLogin(req, res) {
+  const { username, password } = req.body;
+
+  const query = "SELECT password_hash FROM t_users WHERE username = ?;";
+
+  db.query(query, [username], (err, resultat) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Echec lors de la récupération du mot de passe",
+        error: err,
+      });
+    }
+
+    if (resultat.length < 1) {
+      return res.status(500).json({
+        message: "Aucun utilisateur trouvé avec ce nom",
+      });
+    }
+
+    const db_password_hash = resultat[0].password_hash;
+
+    if (password !== db_password_hash) {
+      return res.status(500).json({
+        message: "Le mot de passe ne correspond pas",
+        error: err,
+      });
+    }
+
+    //ok
+    return res.status(200).json({ message: "OK", ok: true });
+  });
+}
+
+module.exports = { getSignUp, postSignUp, postLogin };
