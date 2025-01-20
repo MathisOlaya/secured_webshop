fbd0566abfee35f53a26f6b1f53a2e33d4318710
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-01-20 13:40:59 +0100

    feat: appeler la route qui permet l'ajout d'utilisateur

--- app/public/signup/script.js
+++ app/public/signup/script.js
@@ -1,9 +1,30 @@
-document.getElementById('submitSignup').addEventListener('click', () => {
-    //get input value
-    const username = document.getElementById('usernameInput').value;
-    const password = document.getElementById('passwordInput').value;
+document.getElementById("submitSignup").addEventListener("click", async () => {
+  //get input value
+  const username = document.getElementById("usernameInput").value;
+  const password = document.getElementById("passwordInput").value;
 
-    if(username === '' || password === ''){
-        alert("Un des deux champs est nul. Réessayer");
-    }
-})
+  if (username === "" || password === "") {
+    alert("Un des deux champs est nul. Réessayer");
+  }
+
+  //post option
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  };
+
+  //call post method with options
+  try {
+    const response = await fetch("/user/signup", options);
+
+    //get result
+    const data = await response.json();
+    console.log(data);
+  } catch {}
+});
