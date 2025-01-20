9dcd5c2feea175e1f33db0590e66629c379292a1
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-01-20 12:59:50 +0100

    fix: correction de la connection à la base de donnée, et déplacement de celle-ci

--- app/server.js
+++ app/server.js
@@ -1,55 +1,37 @@
 const express = require("express");
 const https = require("https");
-const fs = require('fs');
-const path = require('path');
-const mysql = require('mysql2')
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
 //add certification file
 const options = {
-    key: fs.readFileSync('certs/key.pem'),
-    cert: fs.readFileSync('certs/cert.crt')
-}
+  key: fs.readFileSync("certs/key.pem"),
+  cert: fs.readFileSync("certs/cert.crt"),
+};
 
-//create connection with db
-// mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'root',
-//     database: 'db_secured_webshop',
-//     port: 6033
-// });
+const userRoute = require("./routes/User");
+app.use("/user", userRoute);
 
-//try to connect
-// connection.connect((err) => {
-//     if (err) {
-//       console.error('Erreur de connexion à la base de données:');
-//       return;
-//     }
-//   });
-
-const userRoute = require('./routes/User');
-app.use('/user', userRoute);
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //default page
-app.get('/', (req, res) => {
-    res.redirect('/login');
-})
+app.get("/", (req, res) => {
+  res.redirect("/login");
+});
 
 //signup page
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'signup', 'signup.html'));
-  });
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "signup", "signup.html"));
+});
 
 //login page (default)
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login', 'login.html'));
-  });
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login", "login.html"));
+});
 
 // Démarrage du serveur
 https.createServer(options, app).listen(443, () => {
-    console.log('Server running on port 443');
+  console.log("Server running on port 443");
 });
