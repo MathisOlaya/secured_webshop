adf70c106fcbebd7cbe315ad72161cd727e62a98
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-01-20 14:41:47 +0100

    feat: appeler l'api afin de vérifier les informations entrée par l'utilisateur
    si tout est bon, le rediriger vers la page affichant son nom

--- app/public/login/script.js
+++ app/public/login/script.js
@@ -1,9 +1,32 @@
-document.getElementById('submitLogin').addEventListener('click', () => {
-    //get input value
-    const username = document.getElementById('usernameInput').value;
-    const password = document.getElementById('passwordInput').value;
+document.getElementById("submitLogin").addEventListener("click", async () => {
+  //get input value
+  const username = document.getElementById("usernameInput").value;
+  const password = document.getElementById("passwordInput").value;
 
-    if(username === '' || password === ''){
-        alert("Un des deux champs est nul. Réessayer");
+  if (username === "" || password === "") {
+    alert("Un des deux champs est nul. Réessayer");
+  }
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  };
+
+  try {
+    const response = await fetch("/user/login", options);
+
+    //get result
+    const data = await response.json();
+
+    if (data.ok) {
+      //redirect to user page
+      window.location.href = `/user/${username}`;
     }
-})
+  } catch {}
+});
