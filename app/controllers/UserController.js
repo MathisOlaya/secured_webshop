08059393aa20828c76d3da9f786eceb27acf3048
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-01-20 13:39:02 +0100

    feat: ajouter le nouvel utilisateur à la table

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -17,11 +17,23 @@ db.connect((err) => {
   }
 });
 
-module.exports = {
-  get: (req, res) => {
-    res.send("User: Sarah Test");
-  },
-  post: (req, res) => {
-    db.query("INSERT INTO t_users (username) VALUES ('TEST')");
-  },
-};
+async function getSignUp(req, res) {
+  res.send("User: Sarah Test");
+}
+
+async function postSignUp(req, res) {
+  const { username, password } = req.body;
+
+  const query = "INSERT INTO t_users (username, password_hash) VALUES (?, ?);";
+  db.query(query, [username, password], (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Erreur lors de la création de l'utilisateur",
+        error: err,
+      });
+    }
+    return res.status(201).json({ message: "OK" });
+  });
+}
+
+module.exports = { getSignUp, postSignUp };
