5bf2f5d95327ed9e706a5fb77ab293334a8aeea6
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-02-03 14:47:21 +0100

    feat: créer le middleware vérifiant l'identité de l'utilisateur

--- app/middleware/Auth.js
+++ app/middleware/Auth.js
@@ -1,3 +1,30 @@
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const path = require("path");
 
-function verifyToken(req, res, next) {}
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+function verifyToken(req, res, next) {
+  const token = req.cookies.jwt_token;
+
+  if (!token) {
+    return res.status(403).json({ message: "No token provided" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid or expired token" });
+    }
+
+    //Vérifier que c'est la bonne personne
+    if (decoded.username !== req.params.username) {
+      return res
+        .status(403)
+        .json({ message: "Accès refusé. Utilisateur non autorisé." });
+    }
+
+    next();
+  });
+}
+
+module.exports = { verifyToken };
