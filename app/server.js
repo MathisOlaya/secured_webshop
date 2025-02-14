80f3ae8d3ea7de47ce3b481f8f2faea93dcdaedd
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-02-14 12:14:48 +0100

    feat: rediriger l'utilisateur déjà connecté  sur la page home

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const https = require("https");
 const fs = require("fs");
 const path = require("path");
+const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -23,17 +24,33 @@ app.set("views", path.join(__dirname, "views"));
 
 //default page
 app.get("/", (req, res) => {
-    res.redirect("/login");
+    const token = req.cookies.jwt_token;
+
+    if (!token) {
+        return res.redirect("/login");
+    }
+
+    const decoded = jwt.decode(token);
+
+    return res.redirect(`/user/${decoded.username}`);
 });
 
 //signup page
 app.get("/signup", (req, res) => {
-    res.render("signup/signup");
+    //if user is already connected, redirect to main
+    if (!req.cookies.jwt_token) {
+        return res.render("signup/signup");
+    }
+    return res.redirect("/");
 });
 
 //login page (default)
 app.get("/login", (req, res) => {
-    res.render("login/login");
+    //if user is already connected, redirect to main
+    if (!req.cookies.jwt_token) {
+        return res.render("login/login");
+    }
+    return res.redirect("/");
 });
 
 // Démarrage du serveur
