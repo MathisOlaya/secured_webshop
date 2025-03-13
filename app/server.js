17d2a40b40f00b6e77c9ade850a51e20c1433f2f
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-03-13 18:49:05 +0100

    fix: resolve error while reading certs

--- app/server.js
+++ app/server.js
@@ -11,8 +11,8 @@ app.use(cookieParser());
 
 //add certification file
 const options = {
-    key: fs.readFileSync("certs/key.pem"),
-    cert: fs.readFileSync("certs/cert.crt"),
+  key: fs.readFileSync("certs/key.pem"),
+  cert: fs.readFileSync("certs/cert.pem"),
 };
 
 const userRoute = require("./routes/User");
@@ -24,36 +24,36 @@ app.set("views", path.join(__dirname, "views"));
 
 //default page
 app.get("/", (req, res) => {
-    const token = req.cookies.jwt_token;
+  const token = req.cookies.jwt_token;
 
-    if (!token) {
-        return res.redirect("/login");
-    }
+  if (!token) {
+    return res.redirect("/login");
+  }
 
-    const decoded = jwt.decode(token);
+  const decoded = jwt.decode(token);
 
-    return res.redirect(`/user/${decoded.username}`);
+  return res.redirect(`/user/${decoded.username}`);
 });
 
 //signup page
 app.get("/signup", (req, res) => {
-    //if user is already connected, redirect to main
-    if (!req.cookies.jwt_token) {
-        return res.render("signup/signup");
-    }
-    return res.redirect("/");
+  //if user is already connected, redirect to main
+  if (!req.cookies.jwt_token) {
+    return res.render("signup/signup");
+  }
+  return res.redirect("/");
 });
 
 //login page (default)
 app.get("/login", (req, res) => {
-    //if user is already connected, redirect to main
-    if (!req.cookies.jwt_token) {
-        return res.render("login/login");
-    }
-    return res.redirect("/");
+  //if user is already connected, redirect to main
+  if (!req.cookies.jwt_token) {
+    return res.render("login/login");
+  }
+  return res.redirect("/");
 });
 
 // Démarrage du serveur
 https.createServer(options, app).listen(443, () => {
-    console.log("Server running on port 443");
+  console.log("Server running on port 443");
 });
